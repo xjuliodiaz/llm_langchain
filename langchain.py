7e022a205366809ce71d6a1b706a56471bfa3226
Julio Diaz <xjuliodiaz@gmail.com>
2026-09-16 12:02:57 -0300

    Second commit

--- langchain.py
+++ langchain.py
@@ -1,7 +1,8 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_cohere import ChatCohere
 from langchain_core.messages import HumanMessage
 from my_models import GEMINI_FLASH
-from my_keys import GEMINI_API_KEY
+from my_keys import GEMINI_API_KEY, COHERE_API_KEY
 from my_helper import encode_image
 
 llm = ChatGoogleGenerativeAI(
@@ -9,6 +10,19 @@ llm = ChatGoogleGenerativeAI(
     model=GEMINI_FLASH,
 )
 
+respuesta = llm.invoke("Cuáles canales colombianos de YT me recomiendas para saber más sobre teléfonos inteligentes?")
+print(f"Gemini: ", respuesta.content)
+
+llm = ChatCohere(
+    cohere_api_key=COHERE_API_KEY
+)
+
+respuesta = llm.invoke([HumanMessage(content="Cuáles canales colombianos de YT me recomiendas para saber más sobre teléfonos inteligentes?")])
+print(f"Cohere: ", respuesta.content)
+
+
+
+"""
 imagen = encode_image("datos/ejemplo_grafico.jpg")
 
 pregunta = "Describe la imagen"
@@ -29,3 +43,5 @@ mensaje = HumanMessage(
 respuesta = llm.invoke([mensaje])
 
 print(respuesta)
+
+"""
